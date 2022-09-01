9a7a015e3a30c311a147c10157a731ae4c81fd80
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-09-01 20:31:34 +0530

    Update memoziation.cpp

--- Dynamic_Programming/striver_dp/DP 5. Maximum Sum of Non-Adjacent Elements | House Robber | 1-D | DP on Subsequences/memoziation.cpp
+++ Dynamic_Programming/striver_dp/DP 5. Maximum Sum of Non-Adjacent Elements | House Robber | 1-D | DP on Subsequences/memoziation.cpp
@@ -1,33 +1,22 @@
-	int solve(int i,int arr[], int dp[])
+	int solve(int n,int arr[],vector<int>&dp)
 	{
-	    if(i == 0)    
-	    return arr[0];
-	    
-	    if(i < 0)
+	    if(n<0)
 	    return 0;
 	    
-	    if(dp[i] != -1)
-	    {
-	        return dp[i];
-	    }
+	    if(n==0)
+	    return arr[n];
 	    
-	    int pick = arr[i] + solve(i - 2, arr , dp);
-	    int notPick = 0 + solve(i - 1, arr , dp);
+	    if(dp[n]!= -1)
+	    return dp[n];
 	    
-	    return dp[i] = max(pick , notPick);
+	    int pick=arr[n]+solve(n-2,arr,dp);
+	    
+	    int notPick=0+solve(n-1,arr,dp);
+	    
+	    return dp[n]=max(pick,notPick);
 	}
-	
 	int findMaxSum(int *arr, int n) {
 	    // code here
-	    if(n == 1)
-	    return arr[0];
-	    
-	    int dp[n];
-	    
-	    memset(dp , -1 , sizeof(dp));
-	    
-	    solve(n - 1, arr , dp);
-	    
-	    
-	    return dp[n - 1];
+	    vector<int> dp(n,-1);
+    	 return solve(n-1,arr,dp);
 	}
