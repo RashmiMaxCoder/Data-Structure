40705a3ce6680dbf71b057f426a48cebfc5342f5
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-09-03 16:57:53 +0530

    Create tabulation.cpp

---
+++ Dynamic_Programming/striver_dp/DP 11. Triangle | Fixed Starting Point and Variable Ending Point | DP on GRIDS/tabulation.cpp
@@ -0,0 +1,18 @@
+int minimumPathSum(vector<vector<int>>& triangle, int n){
+	// Write your code here.
+      vector<vector<int>> dp(n,vector<int>(n,0));
+    for(int i=0;i<n;i++)
+    {
+        dp[n-1][i]= triangle[n-1][i];
+    }
+    for(int i=n-2;i>=0;i--)
+    {
+        for(int j=i;j>=0;j--)
+        {
+             int down=triangle[i][j]+ dp[i+1][j];
+             int down_right=triangle[i][j]+dp[i+1][j+1];
+            dp[i][j]=min(down,down_right);
+        }
+    }
+    return dp[0][0];
+}
