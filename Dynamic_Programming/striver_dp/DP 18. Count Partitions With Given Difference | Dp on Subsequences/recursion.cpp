9bbcd48c81632054a0d63a8d574f6db132436f2f
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-09-05 19:50:37 +0530

    Update recursion.cpp

--- Dynamic_Programming/striver_dp/DP 18. Count Partitions With Given Difference | Dp on Subsequences/recursion.cpp
+++ Dynamic_Programming/striver_dp/DP 18. Count Partitions With Given Difference | Dp on Subsequences/recursion.cpp
@@ -1,3 +1,4 @@
+#include<bits/stdc++.h>
 int solve(int i,int target,vector<int> &arr)
 {
     if(i==0)
@@ -6,17 +7,24 @@ int solve(int i,int target,vector<int> &arr)
                 return 2;
             if(target == 0 ||target == arr[0])
                 return 1;
+
         return 0;
         }
     int nottake = solve(i-1,target,arr);
     int take=0;
     if(arr[i]<=target)
         take=solve(i-1,target-arr[i],arr);
+    
     return nottake+take;
 }
+
 int countPartitions(int n, int d, vector<int> &arr) {
     // Write your code here.
-    return solve(n-1,d,arr);
+    int sum = accumulate(arr.begin(),arr.end(),0);
+    
+    if(sum - d < 0 || ((sum - d)%2))
+        return false;
+    
+    return solve(n-1,(sum - d)/2,arr);
 }
-
 
