0f6217a28b8b58eb919b682ccf643a88e9be1cc8
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-09-06 11:31:02 +0530

    Update ratMaze.cpp

--- graph/ratMaze.cpp
+++ graph/ratMaze.cpp
@@ -2,25 +2,18 @@ void solve(int i, int j , vector<vector<int>> &m,vector<string> &ans, int n, str
     {
         if(i < 0 or j < 0 or i >= n or j >= n or (m[i][j] == 0))
             return;
-        
-        
         if(i == (n - 1) && j == (n - 1) )
         {
             ans.push_back(str);
             return;
         }
-        
-        m[i][j] = 0;
-        
-        
+        m[i][j] = 0; 
         solve(i-1,j,m,ans,n,str + 'U');
         solve(i,j + 1, m , ans , n , str + 'R');
         solve(i + 1,j, m , ans , n , str + 'D');
         solve(i,j - 1, m , ans , n , str + 'L');
         
-        m[i][j] = 1;
-        
-        
+        m[i][j] = 1;     
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
@@ -30,5 +23,36 @@ void solve(int i, int j , vector<vector<int>> &m,vector<string> &ans, int n, str
         
         solve(0 , 0, m , ans, n , "");
         
+        return ans;
+    }
+
+
+
+-------------
+      void solve(int i, int j,vector<vector<int>> &m,vector<string> &ans,int n, string str )
+    {
+       if(i==(n-1) and j==(n-1))
+       {
+           ans.push_back(str);
+           return;
+       }
+       if(i >=0 and j>=0 and i<n and j<n and (m[i][j]==1))
+       {
+        m[i][j]=0;
+        solve(i-1,j,m,ans,n,str +'U');
+        solve(i+1,j,m,ans,n,str +'D');
+        solve(i,j+1,m,ans,n,str +'R');
+        solve(i,j-1,m,ans,n,str +'L');
+        m[i][j]=1;
+       }
+       return;
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<string> ans;
+        if(m[0][0]== 0 or m[n-1][n-1]==0)
+        return ans;
+        
+        solve(0,0,m,ans,n,"");
         return ans;
     }
